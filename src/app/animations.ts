f7742318cf0bb1d80ecad65f4ad723dfd572708b
DiegoWiki <diegocorral.del@gmail.com>
2017-08-10 18:42:17 +0200

    header styling

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -10,7 +10,7 @@ export const slideIn = trigger('slideIn', [
     state('out', style({
         transform: 'translateX(-100%)',
     })),
-    transition('* => in', animate('700ms 150ms ease-in'))
+    transition('* => in', animate('1500ms ease-in'))
 ]);
 export const slideInDelay = trigger('slideInDelay', [
     state('*', style({
@@ -22,21 +22,22 @@ export const slideInDelay = trigger('slideInDelay', [
     state('out', style({
         transform: 'translateX(100%)',
     })),
-    transition('* => in', animate('600ms 720ms ease-in'))
+    transition('* => in', animate('1500ms 1000ms ease-in'))
 ]);
 
 export const slideDown = trigger('slideDown', [
     state('*', style({
         transform: 'translateY(-100%)',
-        backgoundColor: '#f50057'
+        backgoundColor: '#fff'
     })),
     state('in', style({
         transform: 'translateY(0)',
         backgoundColor: '#00e5ff'
     })),
-    transition('* <=> in', animate('1000ms ease-in', keyframes([
-        style({backgroundColor: '#f50057', transform: 'translateY(0px)', offset: 0.3 }),
-        style({backgroundColor: '#ff80ab', transform: 'translateY(-10%)', offset: 0.6 }),
+    transition('* <=> in', animate('1000ms ease-in-out', keyframes([
+        style({backgroundColor: '#fff', transform: 'translateY(-100%)', offset: 0 }),
+        style({backgroundColor: '#00e5ff', transform: 'translateY(0px)', offset: 0.3 }),
+        style({backgroundColor: '#00b8d4', transform: 'translateY(-10%)', offset: 0.6 }),
         style({backgroundColor: '#00e5ff', transform: 'translateY(0px)', offset: 1.0 })
     ])))
 ]);
