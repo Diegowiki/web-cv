25e77efba88b4c9f66483f7cb01528174355bce8
DiegoWiki <diegocorral.del@gmail.com>
2017-08-11 18:36:54 +0200

    fix: animation wait until data is displayed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,28 +3,41 @@ import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/data
 import { Observable } from 'rxjs/Observable';
 import { Header } from './header';
 // animations
-import { slideIn, slideInDelay, slideDown, slideDownAvatar} from '../animations'; // path to your animations.ts file
+import { slideIn, slideInDelay, slideDown, slideDownAvatar } from '../animations'; // path to your animations.ts file
 
 @Component({
   selector: 'dct-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   animations: [
-   slideIn,
-   slideInDelay,
-   slideDown,
-   slideDownAvatar
+    slideIn,
+    slideInDelay,
+    slideDown,
+    slideDownAvatar
   ]
 })
-export class HeaderComponent {
-  header: FirebaseObjectObservable<any>;
-  slideInState = 'in';
-  slideInStateDelay = 'in';
-  slideDownState = 'in';
-  slideDownAvatarState = 'in';
-  constructor(db: AngularFireDatabase) {
-    this.header = db.object('/header');
+export class HeaderComponent implements OnInit {
+  header: FirebaseObjectObservable<Header>;
+  // headerOnInit: FirebaseObjectObservable<Header>;
+  slideInState: string;
+  slideInStateDelay: string;
+  slideDownState: string;
+  slideDownAvatarState: string;
+  constructor(private db: AngularFireDatabase) {
+    this.header = this.db.object('/header');
+    this.header.subscribe(
+      (header) => {
+        this.slideInState = 'in';
+        this.slideDownAvatarState = 'in';
+        this.slideDownState = 'in';
+        this.slideInStateDelay = 'in';
+        console.log(header);
+      },
+      err => console.log(err)
+    );
+
   }
+  ngOnInit(): void {
 
-
+  }
 }
